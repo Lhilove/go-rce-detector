e8546947506707e089103e4155614e30b8f04140
Lhilove <pelumiade92@gmail.com>
2026-05-30 01:55:36 +0100

    converted to a CI/CD pipeline tool

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"gosentinel/rules"
 	"gosentinel/scanner"
 	"log"
+	"os"
 )
 
 func main() {
@@ -16,23 +17,35 @@ func main() {
 		log.Fatal(err)
 	}
 
+	fset := token.NewFileSet()
+	hasCritical := false
+
 	for _, file := range files {
 		node, _, err := scanner.ParseFile(file)
 		if err != nil {
 			log.Printf("failed to parse %s: %v\n", file, err)
 			continue
 		}
+
 		fmt.Printf("Parsed: %s | Package: %s\n", file, node.Name.Name)
 
-		findings := rules.DetectExecCommand(file, node, token.NewFileSet())
+		findings := rules.DetectExecCommand(file, node, fset)
 
 		for _, f := range findings {
-			fmt.Printf("[%s] %s:%d\n",
-				f.Severity,
-				f.File,
-				f.Line,
-			)
+
+			fmt.Printf("[%s] %s:%d\n", f.Severity, f.File, f.Line)
 			fmt.Printf("    %s\n\n", f.Message)
+
+			if f.Severity == "CRITICAL" {
+				hasCritical = true
+			}
 		}
 	}
+
+	if hasCritical {
+		fmt.Println("\n[!] CRITICAL RCE DETECTED - FAILING BUILD")
+		os.Exit(1)
+	}
+
+	os.Exit(0)
 }
